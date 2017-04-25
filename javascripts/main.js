e19ebf15fef510b189e53dcd63bf9f6c57e903ad
Matt Hensley <matt.hensley777@gmail.com>
2017-04-25 17:52:00 -0500

    no

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,23 @@
 console.log("Javascript loading");
+
+$(document).ready(function(){
+	
+	const apiKey = "";
+
+	$("body").on("click","li", (e) => {
+		loadPlaces(e.target.innerHTML).then((data) => {
+			console.log(data);
+		}).catch((error) => {
+			console.log(error);
+		});
+	});
+		const loadPlaces = (droptDownType) => {
+			return new Promise((resolve, reject) => {
+				$.ajax(`https://maps.googleapis.com/maps/api/place/textsearch/xml?query=500&type=${dropdownType}&restaurants+in+Sydney&key=${apiKey}`)
+			.done((data) => resolve(data))
+			.fail((error) => fail(error));
+			
+			});
+		}	
+	});
+
